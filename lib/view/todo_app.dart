41ea2ea56facdc6b64619ae6d7692914978eadbb
Evasoft2020 <evasoft2020@gmail.com>
2022-04-11 19:06:21 +0300

    todo_app create

---
+++ lib/view/todo_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
