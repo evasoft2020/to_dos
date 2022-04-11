e8d60d61fdbfbf3af70ef04dd18a9b89a8cf2cec
Evasoft2020 <evasoft2020@gmail.com>
2022-04-11 19:10:59 +0300

    titlewidget add

--- lib/view/todo_app.dart
+++ lib/view/todo_app.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:to_do/widgets/title_widget.dart';
 
 class TodoApp extends StatelessWidget {
   const TodoApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: ListView(
+        children: [
+          TitleWidget(),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/widgets/title_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TitleWidget extends StatelessWidget {
+  const TitleWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Todos App',
+      style: TextStyle(color: Colors.red, fontSize: 30),
+      textAlign: TextAlign.center,
+    );
+  }
+}
